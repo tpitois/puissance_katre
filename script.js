4681e9331cb52248aeaadbfa95a2e49577824a58
pits <tom.pitois@protonmail.com>
2020-02-01 16:57:40 +0100

    verifierVictoire

---
+++ script.js
@@ -0,0 +1,125 @@
+var plateau = [
+[0, 0, 0, 0, 0, 0, 0],
+[1, 1, 1, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0],
+];
+
+var hauteur_plateau = plateau.length;
+var largeur_plateau = plateau[0].length;
+
+var verifierVictoire = function(plateau, valeur_joueur) {
+	
+	var coords_victoire = [];
+	var victoire = false;
+	
+	//aligneess descendantes
+	
+	for(var y = 0; y < hauteur_plateau-3; y++) {
+		for(var x = 0; x < largeur_plateau-3; x++) {
+			if(plateau[y][x] == valeur_joueur) {
+				
+				var alignees = true;
+				var i = 1;
+				
+				while(alignees == true && i < 4) {
+					if(plateau[y+i][x+i] != valeur_joueur) {
+						alignees = false;
+					}
+					i++;
+				}
+				
+				if(alignees == true) {
+					for(var i = 0; i < 4; i++) {
+						coords_victoire.push([y+i,x+i]);
+					}
+				}
+			}
+		}
+	}
+	
+	//aligneess montantes
+	
+	for(var y = 0; y < hauteur_plateau-3; y++) {
+		for(var x = 3; x < largeur_plateau; x++) {
+			if(plateau[y][x] == valeur_joueur) {
+				
+				var alignees = true;
+				var i = 1;
+				
+				while(alignees == true && i < 4) {
+					if(plateau[y+i][x-i] != valeur_joueur) {
+						alignees = false;
+					}
+					i++;
+				}
+				
+				if(alignees == true) {
+					for(var i = 0; i < 4; i++) {
+						coords_victoire.push([y+i,x-i]);
+					}
+				}
+			}
+		}
+	}
+	
+	//verticales
+	
+	for(var y = 0; y < hauteur_plateau-3; y++) {
+		for(var x = 0; x < largeur_plateau; x++) {
+			if(plateau[y][x] == valeur_joueur) {
+				
+				var alignees = true;
+				var i = 1;
+				
+				while(alignees == true && i < 4) {
+					if(plateau[y+i][x] != valeur_joueur) {
+						alignees = false;
+					}
+					i++;
+				}
+				
+				if(alignees == true) {
+					for(var i = 0; i < 4; i++) {
+						coords_victoire.push([y+i,x]);
+					}
+				}
+			}
+		}
+	}
+	
+	//horizontales
+	
+	for(var y = 0; y < hauteur_plateau; y++) {
+		for(var x = 0; x < largeur_plateau-3; x++) {
+			if(plateau[y][x] == valeur_joueur) {
+				
+				var alignees = true;
+				var i = 1;
+				
+				while(alignees == true && i < 4) {
+					if(plateau[y][x+i] != valeur_joueur) {
+						alignees = false;
+					}
+					i++;
+				}
+				
+				if(alignees == true) {
+					for(var i = 0; i < 4; i++) {
+						coords_victoire.push([y,x+i]);
+					}
+				}
+			}
+		}
+	}
+	
+
+	
+	if(coords_victoire.length > 0) {
+		victoire = true;
+	}
+	
+	return [coords_victoire, victoire];
+};
